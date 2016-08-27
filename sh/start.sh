7712b20a401a868c58eafe5d1edc29b99bcfa0a6
Maxim Kochurov <ferres@213.180.217.206-red.dhcp.yndx.net>
2016-08-27 21:06:18 +0300

    common-sh

--- sh/start.sh
+++ sh/start.sh
@@ -5,4 +5,4 @@ sudo apt-get -y install libfreetype6-dev
 sudo ln -s /usr/include/freetype2/ft2build.h /usr/include/
 sudo apt-get -y install pkg-config
 sudo apt-get -y install libpq-dev python-dev gfortran
-sudo apt-get install liblapack-dev libatlas-dev
+sudo apt-get -y install liblapack-dev libatlas-dev
