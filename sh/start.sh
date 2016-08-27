6ac6fef3c5391069cbcc43115c08f660c561388a
Maxim Kochurov <ferres@213.180.217.206-red.dhcp.yndx.net>
2016-08-27 21:04:20 +0300

    common-sh

---
+++ sh/start.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install gcc
+sudo apt-get -y install python-virtualenv
+sudo apt-get -y install libfreetype6-dev
+sudo ln -s /usr/include/freetype2/ft2build.h /usr/include/
+sudo apt-get -y install pkg-config
+sudo apt-get -y install libpq-dev python-dev gfortran
+sudo apt-get install liblapack-dev libatlas-dev
