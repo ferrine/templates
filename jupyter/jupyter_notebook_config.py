2f304e6516595f75ac751440cc1b85cf9ebf6568
Maxim Kochurov <ferres@213.180.217.206-red.dhcp.yndx.net>
2016-09-30 00:23:10 +0300

    restructured directory

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -1,6 +1,8 @@
 import os
 c = get_config()
+
+########### NB CONVERT ########### 
 c.NbConvertApp.export_format = 'pdf'
 c.TemplateExporter.template_path = ['.', os.path.expanduser('~/.jupyter/templates/')]
 c.Exporter.template_file = 'russian_article'
-
+##################################
